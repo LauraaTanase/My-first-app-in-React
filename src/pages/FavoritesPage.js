c33441fd92da6eb31ec45d9f2003aa949301ab6b
LauraaTanase <zaraflaura@gmail.com>
2024-09-10 15:29:50 -0500

    bootstrap icons added in index.html

--- src/pages/FavoritesPage.js
+++ src/pages/FavoritesPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import FavoriteMealsContainer from "../containers/FavoriteMealsContainer";
+import { Link } from "react-router-dom";
 
 const FavoritesPage = () => {
   const [meals, setMeals] = useState([]);
@@ -12,9 +13,58 @@ const FavoritesPage = () => {
   }, []);
 
   return (
-    <div>
-      <FavoriteMealsContainer data={meals} />
+    <div className="d-flex flex-column min-vh-100">
+      {/* Navbar */}
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            My Favorite Meals
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/favorites">
+                  Favorites
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/home">
+                  Home
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      {/* Main Content */}
+      <main className="flex-grow-1">
+        <div className="container my-5">
+          <FavoriteMealsContainer data={meals} />
+        </div>
+      </main>
+
+      {/* Footer */}
+      <footer className="bg-light py-3 mt-auto">
+        <div className="container text-center">
+          <span className="text-muted">
+            © 2024 My Favorite Meals. All rights reserved.
+          </span>
+        </div>
+      </footer>
     </div>
   );
 };
+
 export default FavoritesPage;
