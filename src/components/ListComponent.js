da297aff1b3fcb5b2a17e8b38bb2e6fad4c1d27a
LauraaTanase <zaraflaura@gmail.com>
2024-09-10 16:18:43 -0500

    Making ListComponent and Homepage responsive

--- src/components/ListComponent.js
+++ src/components/ListComponent.js
@@ -1,131 +1,17 @@
 import React from "react";
-
-const SideAreaComponent = () => {
-  const meals = [
-    { strArea: "American", count: 12 },
-    { strArea: "British", count: 8 },
-    { strArea: "Canadian", count: 6 },
-    { strArea: "Chinese", count: 15 },
-    { strArea: "Croatian", count: 4 },
-    { strArea: "Dutch", count: 3 },
-    { strArea: "Egyptian", count: 7 },
-    { strArea: "Filipino", count: 5 },
-    { strArea: "French", count: 10 },
-    { strArea: "Greek", count: 9 },
-    { strArea: "Indian", count: 13 },
-    { strArea: "Irish", count: 6 },
-    { strArea: "Italian", count: 14 },
-    { strArea: "Jamaican", count: 4 },
-    { strArea: "Japanese", count: 11 },
-    { strArea: "Kenyan", count: 2 },
-    { strArea: "Malaysian", count: 8 },
-    { strArea: "Mexican", count: 10 },
-    { strArea: "Moroccan", count: 3 },
-    { strArea: "Polish", count: 5 },
-    { strArea: "Portuguese", count: 7 },
-    { strArea: "Russian", count: 6 },
-    { strArea: "Spanish", count: 9 },
-    { strArea: "Thai", count: 12 },
-    { strArea: "Tunisian", count: 2 },
-    { strArea: "Turkish", count: 10 },
-    { strArea: "Ukrainian", count: 4 },
-    { strArea: "Unknown", count: 1 },
-    { strArea: "Vietnamese", count: 7 },
-  ];
-
-  return (
-    <div
-      className="d-flex flex-column flex-shrink-0 p-3 bg-light overflow-auto"
-      style={{ width: "250px", height: "100vh" }}
-    >
-      <div className="dropdown">
-        <a
-          className="dropdown-toggle btn btn-light"
-          href="#"
-          id="dropdownMenuButtonArea"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          Areas
-        </a>
-        <ul
-          className="dropdown-menu w-100"
-          aria-labelledby="dropdownMenuButtonArea"
-        >
-          {meals.map((meal, index) => (
-            <li key={index}>
-              <a
-                className="dropdown-item d-flex justify-content-between align-items-center"
-                href="#"
-              >
-                {meal.strArea}
-                <span className="badge bg-secondary">{meal.count}</span>
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-};
-
-const SidebarComponent = () => {
-  const meals = [
-    { strCategory: "Beef" },
-    { strCategory: "Breakfast" },
-    { strCategory: "Chicken" },
-    { strCategory: "Dessert" },
-    { strCategory: "Goat" },
-    { strCategory: "Lamb" },
-    { strCategory: "Miscellaneous" },
-    { strCategory: "Pasta" },
-    { strCategory: "Pork" },
-    { strCategory: "Seafood" },
-    { strCategory: "Side" },
-    { strCategory: "Starter" },
-    { strCategory: "Vegan" },
-    { strCategory: "Vegetarian" },
-  ];
-
-  return (
-    <div className="d-flex flex-column flex-shrink-0 p-3 bg-light overflow-auto">
-      <div className="dropdown">
-        <a
-          className="dropdown-toggle btn btn-light"
-          href="#"
-          id="dropdownMenuButtonCategory"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          Categories
-        </a>
-        <ul
-          className="dropdown-menu w-100"
-          aria-labelledby="dropdownMenuButtonCategory"
-        >
-          {meals.map((meal, index) => (
-            <li key={index}>
-              <a
-                className="dropdown-item d-flex justify-content-between align-items-center"
-                href="#"
-              >
-                {meal.strCategory}
-                <span className="badge bg-secondary">12</span>
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-};
+import SideAreaComponent from "../components/SideAreaComponent";
+import SidebarComponent from "../components/SidebarComponent";
 
 const ListComponent = () => {
   return (
-    <div className="d-flex flex-md-column">
-        <div>
-      <SideAreaComponent />
-      <SidebarComponent />
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-3 col-lg-2">
+          <SideAreaComponent />
+        </div>
+        <div className="col-md-9 col-lg-10">
+          <SidebarComponent />
+        </div>
       </div>
     </div>
   );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MealsContainer } from "../containers/MealsContainer";
-import { mockMeals } from "../assets/mockData";
 import TopbarContainer from "../containers/TopbarContainer";
 import SidebarContainer from "../containers/SidebarContainer";
-export default HomePage;
 
-function HomePage() {
+export default function HomePage() {
+  const searchValue = 'beef';
+  const [data, setData] = useState([]);
+
+  const fetchData = (searchValue) => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`)
+      .then((response) => response.json())
+      .then((responseJSON) => setData(responseJSON));
+  };
+
+  useEffect(() => {
+    fetchData(searchValue);
+  }, [searchValue]);
+
   return (
     <div className="container-fluid">
       <TopbarContainer />
-      <div className="d-flex">
-        <SidebarContainer />
-        <MealsContainer data={mockMeals.meals} />
+      <div className="row">
+        <div className="col-md-3 col-lg-2">
+          <SidebarContainer />
+        </div>
+        <div className="col-md-9 col-lg-10">
+          <MealsContainer data={data.meals} />
+        </div>
       </div>
     </div>
   );
