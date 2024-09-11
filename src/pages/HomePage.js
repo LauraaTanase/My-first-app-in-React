2da71f91fbf8072a0c3ee9cbf51efeb049ba827d
LauraaTanase <zaraflaura@gmail.com>
2024-09-10 19:10:52 -0500

    HomePage modified

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,32 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { MealsContainer } from "../containers/MealsContainer";
 import TopbarContainer from "../containers/TopbarContainer";
 import SidebarContainer from "../containers/SidebarContainer";
 
 export default function HomePage() {
-  const searchValue = 'beef';
-  const [data, setData] = useState([]);
-
-  const fetchData = (searchValue) => {
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`)
-      .then((response) => response.json())
-      .then((responseJSON) => setData(responseJSON));
-  };
-
-  useEffect(() => {
-    fetchData(searchValue);
-  }, [searchValue]);
-
   return (
     <div className="container-fluid">
       <TopbarContainer />
-      <div className="row">
-        <div className="col-md-3 col-lg-2">
-          <SidebarContainer />
-        </div>
-        <div className="col-md-9 col-lg-10">
-          <MealsContainer data={data.meals} />
-        </div>
+      <div className="d-flex">
+        <SidebarContainer/>
+        <MealsContainer/>
       </div>
     </div>
   );
